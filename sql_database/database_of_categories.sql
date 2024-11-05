26fa66a300814f49909ebaf8ebdf34e4b8c30893
Alexander <derznovenko2005@mail.ru>
2024-11-06 02:36:24 +0300

    changed ctagories

--- sql_database/database_of_categories.sql
+++ sql_database/database_of_categories.sql
@@ -1,8 +1,8 @@
 CREATE TABLE sources (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	NAME VARCHAR(100) NOT NULL,
-	tg_id VARCHAR(100) NOT NULL,
 	link VARCHAR(2048) NOT NULL,
 	platform VARCHAR(100) NOT NULL,
+	subscribers INT NOT NULL,
 	category SET('Fashion', 'IT', 'Travel', 'Busines', 'Beauty') NOT NULL
 );
