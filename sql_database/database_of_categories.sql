fa6ad8dfef197b7c5a6b52db010a18464386395b
Alexander <derznovenko2005@mail.ru>
2024-11-16 19:12:01 +0300

    added soursec in db

--- sql_database/database_of_categories.sql
+++ sql_database/database_of_categories.sql
@@ -3,7 +3,7 @@ CREATE TABLE sources (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	NAME VARCHAR(100) NOT NULL,
 	link VARCHAR(2048) NOT NULL,
-	platform VARCHAR(100) NOT NULL,
+	platform SET('Instagam', 'Telegram', 'Tik-tok', 'Vk', 'YouTube', 'Yandex Dzen') NULL,
 	subscribers INT NOT NULL,
 	category SET('Fashion', 'IT', 'Travel', 'Busines', 'Beauty') NOT NULL,
 	UNIQUE KEY name (name)
