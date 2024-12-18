4077afa861e05f99718b7e21c636cf156b3b746d
Artem Genius <armironovtem05@gmail.com>
2024-12-19 01:07:46 +0300

    only few fitches left

--- test_prototype/functions.py
+++ test_prototype/functions.py
@@ -4,187 +4,151 @@ import os
 import sys
 sys.path.append(os.getcwd())
 from AdFast.sql_database import sql_requests
-
 from dataclasses import dataclass
+
+sys.path.append(os.getcwd())
 
 @dataclass
 class Pages:
     page_category: int
     max_page_category: int
-    page_chan: int
-    max_page_chan: int
+    page_result: int
+    max_page_result: int
+    page_socnet: int
+    max_page_socnet: int
+    page_count: int
+    max_page_count: int
 
-# Создание экземпляра класса Pages
-pages_instance = Pages(page_category=1, max_page_category=2, page_chan=1, max_page_chan=0)
+pages_instance = Pages(page_category=1, max_page_category=2, page_result=1, max_page_result=0, max_page_count=2, max_page_socnet=2, page_count=1, page_socnet=1)
+
+@dataclass
+class Button:
+    text: str
+    callback_data: str
+
+@dataclass
+class Lists:
+    category: list
+    socnet: list
+    count: list
+
+lst = Lists(category=["Fashion", "IT", "Travel", "Business", "Beauty"], count=['10.000-', '10.000-50.000', '50.000-100.000', '100.000-500.000', '500.000-1.000.000', '1.000.000+'], socnet=['Instagram', 'Telegram', 'TikTok', 'VK', 'YouTube', 'Yandex Dzen'])
 
 def start_func(message, bot):
     markup = types.InlineKeyboardMarkup()
-    start_button = types.InlineKeyboardButton("Начать поиск места для рекламы", callback_data="start_search")
-    markup.add(start_button)
-    bot.send_message(message.chat.id,
-f"""Приветствую, {message.from_user.first_name}!
-Это тестовый прототип дизайна бота FastAd.
-Дальнейший функционал будет добавлен позже.
-А пока можно потыкать на кнопки ниже :)""", 
-reply_markup=markup)
+    markup.row(*create_inline_buttons([Button("Начать поиск места для рекламы", "start_search")]))
+    bot.send_message(message.chat.id, f"Приветствую, {message.from_user.first_name}! Это тестовый прототип дизайна бота FastAd. Дальнейший функционал будет добавлен позже. А пока можно потыкать на кнопки ниже :)", reply_markup=markup)
     sql_requests.adding_user_in_database(message.from_user.first_name, message.from_user.id)
 
+def create_inline_buttons(buttons):
+    result = []
+    for button in buttons:
+        result.append(types.InlineKeyboardButton(button.text, callback_data=button.callback_data))
+    return result
 
-def page_category(call, bot):
+def create_navigation_buttons(current_page, max_page, item_type):
+    buttons = []
+    if current_page > 1:
+        buttons.append(Button('Назад', f'back_page {item_type}'))
+    if current_page < max_page:
+        buttons.append(Button('Вперед', f'forward_page {item_type}'))
+    buttons = create_inline_buttons(buttons)
+    return buttons
+
+def update_pages(call, bot):
+    bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id, reply_markup=None)
+    bot.delete_message(call.message.chat.id, call.message.message_id)
+
+def page_selection(call, bot, items, item_type):
+    update_pages(call, bot)
+
     markup = types.InlineKeyboardMarkup()
-    lst_categories = ["Fashion", "IT", "Travel", "Busines", "Beauty"]
-    rows = [] # создаю пустой массив для кнопок
-    start_ind = (pages_instance.page_category - 1) * 4 # определяю стартовый индекс из списка
-    ind = start_ind
-    while (ind < len(lst_categories) and ind < start_ind + 4): # Добавляю либо 4 кнопки по 2 в строку, либо иду до конца списка
-        if len(rows) == 2:
-            markup.row(rows[0], rows[1])
-            rows = []
-        category_btn = types.InlineKeyboardButton(lst_categories[ind], callback_data=f'category {lst_categories[ind]}')
-        rows.append(category_btn)
-        ind += 1
-    # добавляю оставшиеся в массиве кнопки в последнюю строку
-    if len(rows) == 2:
-        markup.row(rows[0], rows[1])
-    else:
-        markup.row(rows[0])
-    # определяю и добавляю нужные "кнопки перемещения" по страницам
-    current_page = pages_instance.page_category
-    max_page = pages_instance.max_page_category
 
-    match (current_page, max_page):
-        case (1, max_page) if max_page > 1:  # Если на первой странице и есть следующая
-            move_forward_btn = types.InlineKeyboardButton('Вперед', callback_data='forward_page_category')
-            markup.row(move_forward_btn)
-        case (_, _) if current_page == max_page and max_page > 1:  # Если на последней странице и есть предыдущая
-            move_back_btn = types.InlineKeyboardButton('Назад', callback_data='back_page_category')
-            markup.row(move_back_btn)
-        case (current_page, max_page) if current_page > 1 and current_page < max_page:  # Если не первая и не последняя страница
-            move_forward_btn = types.InlineKeyboardButton('Вперед', callback_data='forward_page_category')
-            move_back_btn = types.InlineKeyboardButton('Назад', callback_data='back_page_category')
-            markup.row(move_back_btn, move_forward_btn)
+    # Определяем страницу
+    if item_type == 'result':
+        pages_instance.max_page_result = len(items) // 4 + (1 if len(items) % 4 != 0 else 0)
+     
+    current_page = getattr(pages_instance, f'page_{item_type}', None)
+    max_page = getattr(pages_instance, f'max_page_{item_type}', None)
+    items_temp = items[(current_page - 1)* 4 : min((current_page - 1) * 4 + 4, len(items))]
+
+
+    for i in range(0, len(items_temp), 2):
+        buttons_on_page = []
+        button1 = Button(items_temp[i], f'{item_type} {i + 1}' if item_type == 'result' else f'{item_type} {items_temp[i]}')
+        buttons_on_page.append(button1)
+        if i + 1 < len(items_temp):
+            button2 = Button(items_temp[i + 1], f'{item_type} {i + 2}' if item_type == 'result' else f"{item_type} {items_temp[i + 1]}")
+            buttons_on_page.append(button2)
+        markup.row(*create_inline_buttons(buttons_on_page))
+        
     
-    bot.send_message(call.message.chat.id, 'Выбирите категорию ресурса, где хотите разместить рекламу', reply_markup=markup)
+    # Добавляем кнопки навигации
+    markup.row(*create_navigation_buttons(current_page, max_page, item_type))
 
-def category_callback_func(call, bot):
-    bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id, reply_markup=None)
-    bot.delete_message(call.message.chat.id, call.message.message_id)
-    bot.answer_callback_query(call.id, text=f"Теперь выберите количество подписчиков:")
-    # Создаем новую клавиатуру
-    markup = types.InlineKeyboardMarkup()
+    # Добавляем кнопку "Отмена"
+    markup.add(types.InlineKeyboardButton("Отмена", callback_data=f'cancel_{item_type}'))
 
-    # Определяем названия кнопок, которые будут добавлены в клавиатуру
-    button_names = ['10.000-', '10.000-50.000', '50.000-100.000', '100.000-500.000', '500.000-1.000.000', '1.000.000+']
-    # Добавляем кнопки на клавиатуру
-    for i in range(0, len(button_names), 2):
-        button1 = types.InlineKeyboardButton(button_names[i], callback_data=f"count {button_names[i]}")
-        button2 = types.InlineKeyboardButton(button_names[i + 1], callback_data=f"count {button_names[i + 1]}")
-        markup.row(button1, button2)
-    button_back = types.InlineKeyboardButton('Назад к категориям',callback_data=f'back_count')
-    markup.row(button_back)
-    bot.send_message(call.message.chat.id, 'Выберите желаемо количество аудитории', reply_markup=markup)
+    # Определяем текст сообщения в зависимости от item_type
+    item_texts = {
+        'category': 'Выберите категорию:',
+        'count': 'Выберите количество подписчиков:',
+        'socnet': 'Выберите социальную сеть:',
+        'result': 'Выберите канал:'
+    }
+    
+    message_text = item_texts.get(item_type, "Выберите элемент:")  # Значение по умолчанию
 
+    bot.send_message(call.message.chat.id, message_text, reply_markup=markup)
 
-def back_count_callback_func(call, bot):
-    pages_instance.page_category = 1
-    page_category(call, bot)
-    bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id, reply_markup=None)
-    bot.delete_message(call.message.chat.id, call.message.message_id)
+def cancell_func(call, bot, item_type):
+    match item_type:
+        case 'count':
+            # Если item_type равно 'count', меняем его на 'category'
+            new_item_type = 'category'
+            page_selection(call, bot, lst.category, new_item_type)
+        case 'result':
+            # Если item_type равно 'result', меняем его на 'socnet'
+            new_item_type = 'socnet'
+            page_selection(call, bot, lst.socnet, new_item_type)
+        case 'category':
+            # Если item_type равно 'category', вызываем start_func
+            start_func(call.message, bot)
+        case 'socnet':
+            # Если item_type равно 'socnet', меняем его на 'count'
+            new_item_type = 'count'
+            page_selection(call, bot, lst.count, new_item_type)
 
-
-def count_callback_func(call, bot):
-    bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id, reply_markup=None)
-    bot.delete_message(call.message.chat.id, call.message.message_id)
-    bot.answer_callback_query(call.id, text=f"Выберите соц-сеть, в которой хотите разместить рекламу:")
-    # Создаем новую клавиатуру
-    markup = types.InlineKeyboardMarkup()
-
-    # Определяем названия кнопок, которые будут добавлены в клавиатуру
-    button_names = ['Instagam', 'Telegram', 'Tik-tok', 'Vk', 'YouTube', 'Yandex Dzen']
-    # Добавляем кнопки на клавиатуру
-    for i in range(0, len(button_names), 2):
-        button1 = types.InlineKeyboardButton(button_names[i], callback_data=f"socnet {button_names[i]}")
-        button2 = types.InlineKeyboardButton(button_names[i + 1], callback_data=f"socnet {button_names[i + 1]}")
-        markup.row(button1, button2)
-    button_back = types.InlineKeyboardButton('Назад к кол-ву аудитории',callback_data=f'back_social_network')
-    markup.row(button_back)
-    bot.send_message(call.message.chat.id, 'Выберите соц-сеть, в которой хотите разместить рекламу:', reply_markup=markup)
 
 def socnet_callback_func(call, bot, choise):
-    bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id, reply_markup=None)
-    bot.delete_message(call.message.chat.id, call.message.message_id)
-    l_border = 0
-    r_border = 0
-    if choise['count'][-1] == '-':
-        l_border = 0
-        r_border = 10000
-    elif choise['count'][-1] == '+':
-        l_border = 1000000
-        r_border = 100000000
-    else:
-        temp_count = list(choise['count'].split('-'))
-        l_border = int(temp_count[0].replace('.', ''))
-        r_border = int(temp_count[1].replace('.', ''))
-    lst_channels = sql_requests.selecting_info_of_source('name', choise['category'], l_border, r_border, choise['socnet'])
-    page_channels_func(call, bot, choise, lst_channels)
-
-def page_channels_func(call, bot, choise, lst_chan):
-    markup = types.InlineKeyboardMarkup()
-    start_ind = (pages_instance.page_chan - 1) * 4 # определяю стартовый индекс из списка
-    pages_instance.max_page_chan = len(lst_chan) // 4 + int((len(lst_chan) % 4 != 0))
-    ind = start_ind
-    while (ind < len(lst_chan) and ind < start_ind + 4): # Добавляю либо 4 кнопки по 1 в строке, либо иду до конца списка
-        chan_btn = types.InlineKeyboardButton(lst_chan[ind], callback_data=f'result_{ind + 1}')
-        markup.row(chan_btn)
-        ind += 1
-    # определяю и добавляю нужные "кнопки перемещения" по страницам
-    current_page = pages_instance.page_chan
-    max_page = pages_instance.max_page_chan
-
-    match (current_page, max_page):
-        case (1, max_page) if max_page > 1:  # Если на первой странице и есть следующая
-            move_forward_btn = types.InlineKeyboardButton('Вперед', callback_data='forward_page_chan')
-            markup.row(move_forward_btn)
-        case (_, _) if current_page == max_page and max_page > 1:  # Если на последней странице и есть предыдущая
-            move_back_btn = types.InlineKeyboardButton('Назад', callback_data='back_page_chan')
-            markup.row(move_back_btn)
-        case (current_page, max_page) if current_page > 1 and current_page < max_page:  # Если не первая и не последняя страница
-            move_forward_btn = types.InlineKeyboardButton('Вперед', callback_data='forward_page_chan')
-            move_back_btn = types.InlineKeyboardButton('Назад', callback_data='back_page_chan')
-            markup.row(move_back_btn, move_forward_btn)
     
-    bot.send_message(call.message.chat.id, 'Вот кандидаты на размещение рекламы:', reply_markup=markup)
+    l_border, r_border = determine_borders(choise['count'])
+    
+    lst_channels = sql_requests.selecting_info_of_source('name', choise['category'], l_border, r_border, choise['socnet'])
+    
+    page_selection(call, bot, lst_channels, 'result')
 
+def determine_borders(count_choice):
+    if count_choice[-1] == '-':
+        return 0, 10000
+    elif count_choice[-1] == '+':
+        return 1000000, 100000000
+    else:
+        temp_count = list(count_choice.split('-'))
+        return int(temp_count[0].replace('.', '')), int(temp_count[1].replace('.', ''))
 
-#реализация вывода инфо о канале
 
 def result_callback_func(call, bot, choice):
-    # Извлекаем номер из call.data
-    # Предполагаем, что call.data имеет формат 'result_{num}'
-    num = int(call.data.split('_')[1])
-
-    # Вызываем функцию getting_info_of_source с аргументами choice и num
+    num = int(call.data.split(' ')[1])
+    
     channel_info = sql_requests.getting_info_of_source(choice, num)
-
-    # Предполагаем, что channel_info — это список с нужными данными
-    name = channel_info[0]         # Название канала
-    amount = channel_info[1]       # Количество подписчиков
-    description = channel_info[2]  # Описание канала
-    contacts = channel_info[3]     # Контакты для связи
-
-    # Формируем сообщение для отправки
-    message = f"Название: '{name}'\n" \
-              f"Подписчики: {amount}\n" \
-              f"Описание:\n{description}\n" \
-              f"Контакты:\n{contacts}"
-
-    # Отправляем сообщение пользователю
-    bot.send_message(call.message.chat.id, message)
-
-    # Создаем кнопку "Вернуться к началу"
-    back_button = types.InlineKeyboardButton("Вернуться к началу", callback_data='start_search')
+    
+    message = f"Название: '{channel_info[0]}'\nПодписчики: {channel_info[1]}\nОписание:\n{channel_info[2]}\nКонтакты:\n{channel_info[3]}"
+    
+    back_button = Button("Вернуться к началу", "start_search")
+    
     markup = types.InlineKeyboardMarkup()
-    markup.add(back_button)
-
-    # Отправляем кнопку пользователю
+    markup.row(*create_inline_buttons([back_button]))
+    
+    bot.send_message(call.message.chat.id, message)
     bot.send_message(call.message.chat.id, "Выберите действие:", reply_markup=markup)
