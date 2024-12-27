6852196422186254434154c5217bf4598de35132
Alexander <derznovenko2005@mail.ru>
2024-12-27 17:38:39 +0300

    changed connection to db

--- sql_database/sql_requests.py
+++ sql_database/sql_requests.py
@@ -1,92 +1,105 @@
 import mysql.connector
+from mysql.connector import connect, Error
 import os
 import sys
 sys.path.append(os.getcwd())
 import config as config
 
 
-def adding_user_in_database(user_name, user_id):
-    try:
-        insert_user = f"""INSERT INTO users_adfast (name, user_id)
-        VALUES
-            ('%s', '%s');
-        """ % (user_name, user_id)
-        cur = connection.cursor()
-        cur.execute(insert_user)
-        connection.commit()
-    except mysql.connector.Error as e:
-        print(e)
+class DatabaseConnection:
+    def __init__(self, host, user, password, database):
+        self._host = host
+        self._user = user
+        self._password = password
+        self._database = database
+        self._connection = None
 
+    def connect(self):
+        try:
+            self._connection = mysql.connector.connect(
+                host=self._host,
+                user=self._user,
+                password=self._password,
+                database=self._database
+            )
+            if self._connection.is_connected():
+                print("Успешное подключение к базе данных")
+        except Error as e:
+            print(f"Ошибка при подключении к базе данных: {e}")
 
-#выбор столбца из таблицы, где %s означает имя столбца
-# пример работы функции:
-# print(selecting_info_of_source('name'))
-# >>> [('CodeCamp',), ('easyoffer',)]
-def selecting_info_of_source(command, category, subscribers_begin, subscribers_end, platform):
-    try:
-        selecting = f"""SELECT %s FROM sources
-            WHERE category = "%s" and subscribers BETWEEN %d and %d and platform = "%s"
-            ORDER BY subscribers DESC
-        """ % (command, category, subscribers_begin, subscribers_end, platform)
-        cur = connection.cursor()
-        cur.execute(selecting)
-        rows = cur.fetchall()
-        connection.commit()
-        # Преобразуем rows в список
-        result_list = [row for row in rows]  # Список кортежей
-        # Если нужно получить плоский список значений из первого столбца:
-        flat_list = [row[0] for row in rows]  # Плоский список значений
-        return flat_list
-    except mysql.connector.Error as e:
-        print(e)
+    def close(self):
+        """Закрывает соединение с базой данных."""
+        if self._connection and self._connection.is_connected():
+            self._connection.close()
+            print("Соединение с базой данных закрыто")
+    
+    def adding_user_in_database(self, user_name, user_id):
+        try:
+            insert_user = f"""INSERT INTO users_adfast (name, user_id)
+            VALUES
+                ('%s', '%s');
+            """ % (user_name, user_id)
+            cur = self._connection.cursor()
+            cur.execute(insert_user)
+            self._connection.commit()
+        except mysql.connector.Error as e:
+            print(e)
 
-def adding_source_in_database(name, link, platform, subscribers, category):
-    try:
-        insert_source = f"""INSERT INTO sources (name, link, platform, subscribers, category)
-        VALUES
-            ('%s', '%s', '%s', %d, '%s');
-        """ % (name, link, platform, subscribers, category)
-        cur = connection.cursor()
-        cur.execute(insert_source)
-        connection.commit()
-    except mysql.connector.Error as e:
-        print(e)
+    #выбор столбца из таблицы, где %s означает имя столбца
+    # пример работы функции:
+    # print(selecting_info_of_source('name'))
+    # >>> [('CodeCamp',), ('easyoffer',)]
+    def selecting_info_of_source(self, command, category, subscribers_begin, subscribers_end, platform):
+        try:
+            selecting = f"""SELECT %s FROM sources
+                WHERE category = "%s" and subscribers BETWEEN %d and %d and platform = "%s"
+                ORDER BY subscribers DESC
+            """ % (command, category, subscribers_begin, subscribers_end, platform)
+            cur = self._connection.cursor()
+            cur.execute(selecting)
+            rows = cur.fetchall()
+            self._connection.commit()
+            # Преобразуем rows в список
+            result_list = [row for row in rows]  # Список кортежей
+            # Если нужно получить плоский список значений из первого столбца:
+            flat_list = [row[0] for row in rows]  # Плоский список значений
+            return flat_list
+        except mysql.connector.Error as e:
+            print(e)
 
-
-def getting_info_of_source(choise, num):
-    l_border = 0
-    r_border = 0
-    if choise['count'][-1] == '-':
+    def getting_info_of_source(self, choise, num):
         l_border = 0
-        r_border = 10000
-    elif choise['count'][-1] == '+':
-        l_border = 1000000
-        r_border = 100000000
-    else:
-        temp_count = list(choise['count'].split('-'))
-        l_border = int(temp_count[0].replace('.', ''))
-        r_border = int(temp_count[1].replace('.', ''))
-    try:
-        selecting = f"""SELECT name, subscribers, description, contact FROM sources
-            WHERE category = "%s" and subscribers BETWEEN %d and %d and platform = "%s"
-            ORDER BY subscribers DESC
-        """ % (choise['category'], l_border, r_border, choise['socnet'])
-        cur = connection.cursor()
-        cur.execute(selecting)
-        rows = cur.fetchall()
-        connection.commit()
-        # Преобразуем rows в список
-        result_list = [row for row in rows]  # Список кортежей
-        return result_list[num]
-    except mysql.connector.Error as e:
-        print(e)
+        r_border = 0
+        if choise['count'][-1] == '-':
+            l_border = 0
+            r_border = 10000
+        elif choise['count'][-1] == '+':
+            l_border = 1000000
+            r_border = 100000000
+        else:
+            temp_count = list(choise['count'].split('-'))
+            l_border = int(temp_count[0].replace('.', ''))
+            r_border = int(temp_count[1].replace('.', ''))
+        try:
+            selecting = f"""SELECT name, subscribers, description, contact FROM sources
+                WHERE category = "%s" and subscribers BETWEEN %d and %d and platform = "%s"
+                ORDER BY subscribers DESC
+            """ % (choise['category'], l_border, r_border, choise['socnet'])
+            cur = self._connection.cursor()
+            cur.execute(selecting)
+            rows = cur.fetchall()
+            self._connection.commit()
+            # Преобразуем rows в список
+            result_list = [row for row in rows]  # Список кортежей
+            return result_list[num]
+        except mysql.connector.Error as e:
+            print(e)
 
+
+        
 
 # Connect to server
-connection = mysql.connector.connect(
-    host=config.my_sql_host,
-    user=config.my_sql_user,
-    password=config.my_sql_password,
-    database=config.my_sql_database
-    )
+connection = DatabaseConnection(config.my_sql_host, config.my_sql_user, config.my_sql_password, config.my_sql_database)
+
+connection.connect()
 
--- test_prototype/prototype.py
+++ test_prototype/prototype.py
@@ -1,68 +1,46 @@
-#подключение библиотек
 import telebot
 from telebot import types
 import os
 import sys
 sys.path.append(os.getcwd())
 from sql_database import sql_requests
-import functions
-#подключение бота к коду через ключ
+from test_prototype.functions import lst, start_func, page_selection, socnet_callback_func, result_callback_func, cancell_func
+
+
 bot = telebot.TeleBot('7224861304:AAEg-57ikPQaxWCBGc7f2E-w79WiCXV7uIU')
+choise = {'category': '', 'count': '', 'socnet': ''}
 
-choise = {"category" : "", "count" : "", "socnet" : ""}
-#выполнение команды /start, создание кнопки к сообщению, добавление пользователя в бд
 @bot.message_handler(commands=['start'])
-def start(message):
-    functions.start_func(message, bot)
-    
-#ответ на кнопку "начать поиск рекламы", создание cтраниц с категориями
-@bot.callback_query_handler(func=lambda call: call.data == 'start_search')
-def callback_start(call):
-    functions.page_category(call, bot)
+def handle_start(message):
+    start_func(message, bot)
 
-#переключение на страницу вперед
-@bot.callback_query_handler(func=lambda call: call.data == 'forward_page_category')
-def forward_callback(call):
-    functions.forward_callback_func(call, bot)
+@bot.callback_query_handler(func=lambda call: call.data.startswith('start_search'))
+def start_callback(call):
+    page_selection(call, bot, lst.category, 'category')
 
-#переключение на страницу назад   
-@bot.callback_query_handler(func=lambda call: call.data == 'back_page_category')
-def back_callback(call):
-    functions.back_callback_func(call, bot)
-    
-#Выбор количества подписчиков
+@bot.callback_query_handler(func=lambda call: call.data.startswith('cancel'))
+def cancel(call):
+    cancell_func(call, bot, item_type=(call.data.split('_')[1]))
+
 @bot.callback_query_handler(func=lambda call: call.data.startswith('category'))
 def category_callback(call):
-    choise['category'] = call.data[9:]
-    functions.category_callback_func(call, bot)
+    choise['category'] = call.data.split(' ')[1] # Получаем выбранную категорию
+    page_selection(call, bot, lst.count, 'count')
 
-#Возврат при неправильном выборе кол-ва подписчиков
-@bot.callback_query_handler(func=lambda call: call.data == 'back_count')
-def back_count_callback(call):
-    functions.back_count_callback_func(call, bot)
-    
-    
-#Выбор соц-сети
 @bot.callback_query_handler(func=lambda call: call.data.startswith('count'))
 def count_callback(call):
-    choise['count'] = call.data[6:]
-    functions.count_callback_func(call, bot)
+    choise['count'] = call.data.split(' ')[1] # Получаем выбранное кол-во
+    page_selection(call, bot, lst.socnet, 'socnet')
 
-#Возврат при неправильном выборе соц сети
-@bot.callback_query_handler(func=lambda call: call.data == 'back_social_network')
-def back_socnet_callback(call):
-    functions.category_callback_func(call, bot)
-
-#Выдача списка после выбора соц сети
 @bot.callback_query_handler(func=lambda call: call.data.startswith('socnet'))
 def socnet_callback(call):
-    choise['socnet'] = call.data[7:]
-    print(choise['category'], choise['count'], choise['socnet'])
-    functions.socnet_callback_func(call, bot)
+    choise['socnet'] = call.data.split(' ')[1]  # Получаем выбранную соцсеть
+    socnet_callback_func(call, bot, choise)
+
+@bot.callback_query_handler(func=lambda call: call.data.startswith('result'))
+def result_callback(call):
+      # Заполните нужные данные для choice
+    result_callback_func(call, bot, choise)
 
 bot.polling()
-
-# Close connection
-
-
 sql_requests.connection.close()
